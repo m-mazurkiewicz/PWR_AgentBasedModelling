94a3593c01e1082a64471cb9460bb4d0f8c03ecb
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-10-18 14:29:02 +0200

    minor fix

--- List1/my_version/percolation.py
+++ List1/my_version/percolation.py
@@ -11,10 +11,10 @@ def find_percolation_threshold(height, width, density=.5):
             probability +=1
     return probability/100
 
-def calculate_threshold_for_different_densities(lowest_density=.2, highest_density=.7, number_of_densities_to_check=50):
+def calculate_threshold_for_different_densities(lowest_density=.2, highest_density=.7, number_of_densities_to_check=50, width=20, height=20):
     list_of_thresholds=dict()
     for density in np.linspace(lowest_density, highest_density, number_of_densities_to_check, endpoint=False):
-        list_of_thresholds[density]=find_percolation_threshold(20, 20, density)
+        list_of_thresholds[density]=find_percolation_threshold(width, height, density)
     return list_of_thresholds
 
 def print_thresholds(dict_of_thresholds):
