72e0193ef69eac6f5c4cb1f331fb52e333d92fcc
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-10-18 13:58:22 +0200

    Clean code in model file

--- List1/my_version/main.py
+++ List1/my_version/main.py
@@ -1,17 +1,17 @@
 from List1.my_version.model import ForestFire
-# from List1.my_version.percolation import find_percolation_threshold
+from List1.my_version.percolation import find_percolation_threshold, calculate_threshold_for_different_densities, print_thresholds
 # from List1.my_version.test import Solution
 
 
 def main():
-    model = ForestFire(5, 5, .45)
-    #model.print()
-    model.burn_forest()
-    model.print()
-    print('\n')
-    #model.hohesh_kopelman()
-    model.hoshen_kapelman_alghoritm()
-    model.print()
+    # model = ForestFire(10, 10, .5)
+    # #model.print()
+    # model.burn_forest()
+    # model.print()
+    # print('\n')
+    # #model.hohesh_kopelman()
+    # model.hoshen_kapelman_alghoritm()
+    # model.print()
     #print(model.labels)
     #print(model.biggest_cluster())
     # model.animate_and_simulate('elo')
@@ -21,8 +21,8 @@ def main():
     # result = claster.largestIsland()
     # print(result)
     # claster.print()
-    #threshold = find_percolation_threshold(20, 20)
-    #print(threshold)
+    print_thresholds(calculate_threshold_for_different_densities())
+    #print(calculate_threshold_for_different_densities())
 
 if __name__ == '__main__':
     main()
