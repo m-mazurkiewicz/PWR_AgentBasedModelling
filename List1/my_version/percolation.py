781610a20ca2d703ea70648a7b0701ed40c68b44
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-10-18 13:57:24 +0200

    percolation threshold - complete assignment from list, add plot

--- List1/my_version/percolation.py
+++ List1/my_version/percolation.py
@@ -1,9 +1,23 @@
 from List1.my_version.model import ForestFire
 import numpy as np
+import matplotlib.pyplot as plt
 
-def find_percolation_threshold(height, width):
-    for density in np.linspace(.01, 1, 99, endpoint=False):
+def find_percolation_threshold(height, width, density=.5):
+    probability=0
+    for i in range(100):
         forest = ForestFire(height, width, density)
         forest.burn_forest()
         if forest.check_if_second_edge_is_reached():
-            return density
+            probability +=1
+    return probability/100
+
+def calculate_threshold_for_different_densities(lowest_density=.2, highest_density=.7, number_of_densities_to_check=50):
+    list_of_thresholds=dict()
+    for density in np.linspace(lowest_density, highest_density, number_of_densities_to_check, endpoint=False):
+        list_of_thresholds[density]=find_percolation_threshold(20, 20, density)
+    return list_of_thresholds
+
+def print_thresholds(dict_of_thresholds):
+    x, y = zip(*dict_of_thresholds.items())
+    plt.plot(x, y)
+    plt.show()
