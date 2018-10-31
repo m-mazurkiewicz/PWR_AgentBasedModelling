0a5f2881f66db99b6a7f14f363675e92387523e4
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-10-31 17:37:04 +0100

    living neighbours method

--- List3/game_of_life.py
+++ List3/game_of_life.py
@@ -17,3 +17,14 @@ class GameOfLife:
             for y in range(self.height):
                 if random.random() < self.density:
                     self.grid[x][y] = 1
+
+    def count_living_neighbours(self, cell):
+        x = cell[0]
+        y = cell[1]
+        living_neighbors = 0
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                if (x + i >=0) and (x + i < self.width) and (y + j >=0) and (y + j < self.height) and i != 0 and j != 0:
+                    if self.grid[x + i][y + j] == 1:
+                        living_neighbors += 1
+        return living_neighbors
