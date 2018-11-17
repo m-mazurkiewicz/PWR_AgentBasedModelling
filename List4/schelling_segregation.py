10f19cadf951dae99a8cc9125b4d143c3a53e07f
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-11-17 17:44:34 +0100

    trying to make plots

--- List4/schelling_segregation.py
+++ List4/schelling_segregation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 from sklearn.neighbors import NearestNeighbors
+from matplotlib import pyplot as plt
+from copy import deepcopy as copy
 
 
 class ShellingSegregation:
@@ -69,13 +71,24 @@ class ShellingSegregation:
 
     def whole_process(self):
         self.agents_willing_to_move = True
+        self.history_ = []
+        self.history_alt_ = []
         number_of_cycles = 0
         while self.agents_willing_to_move:
+            blue_coordinates = []
+            red_coordinates = []
+            for index in self.blue:
+                blue_coordinates.append(copy(self.occupied_fields[index]))
+            for index in self.red:
+                red_coordinates.append(copy(self.occupied_fields[index]))
+            self.history_alt_.append((blue_coordinates,red_coordinates))
+            self.history_.append((copy(self.blue), copy(self.red), copy(self.occupied_fields)))
             self.agents_willing_to_move = False
             for coordinates in list(self.occupied_fields.values()): #we can't loop over dict that is changing during iteration. This approach works, but I'm not completely sure if it fulfills lists assumptions
                 self.single_step(coordinates[0], coordinates[1])
                 #self.single_step(self.occupied_fields[key][0], self.occupied_fields[key][1])
             number_of_cycles += 1
+        self.history_.append((copy(self.blue), copy(self.red), copy(self.occupied_fields)))
         print(number_of_cycles)
 
     def segregation_index(self):
@@ -90,10 +103,78 @@ class ShellingSegregation:
             similar_neighbour_index += self.find_number_of_neighbours_of_the_same_type(agents_type, self.make_agents_indices_from_coordinates(list_of_agent_neighbours)) / len(list_of_agent_neighbours)
         return similar_neighbour_index / len(self.occupied_fields.values())
 
+    def plot(self):
+        length_of_simulation = len(self.history_)
+        plt.figure(figsize=(6,4))
+        plt.subplot(231)
+        self.plot_history(self.history_[0])
+        plt.title('1')
+        # plt.subplot(232)
+        # self.plot_history(self.history_[int(np.floor(length_of_simulation/5))])
+        # plt.title(str(int(np.floor(length_of_simulation/5))))
+        # plt.subplot(233)
+        # self.plot_history(self.history_[2*int(np.floor(length_of_simulation/5))])
+        # plt.title(str(2*int(np.floor(length_of_simulation/5))))
+        # plt.subplot(234)
+        # self.plot_history(self.history_[3*int(np.floor(length_of_simulation/5))])
+        # plt.title(str(3*int(np.floor(length_of_simulation/5))))
+        # plt.subplot(235)
+        # self.plot_history(self.history_[4*int(np.floor(length_of_simulation/5))])
+        # plt.title(str(4*int(np.floor(length_of_simulation/5))))
+        plt.subplot(236)
+        self.plot_history(self.history_[-1])
+        plt.title(str(length_of_simulation))
+        plt.show()
+        plt.close()
+
+    def plot_alt(self):
+        length_of_simulation = len(self.history_)
+        plt.figure(figsize=(6,4))
+        plt.subplot(231)
+        self.plot_history_alt(self.history_[0])
+        plt.title('1')
+        # plt.subplot(232)
+        # self.plot_history(self.history_[int(np.floor(length_of_simulation/5))])
+        # plt.title(str(int(np.floor(length_of_simulation/5))))
+        # plt.subplot(233)
+        # self.plot_history(self.history_[2*int(np.floor(length_of_simulation/5))])
+        # plt.title(str(2*int(np.floor(length_of_simulation/5))))
+        # plt.subplot(234)
+        # self.plot_history(self.history_[3*int(np.floor(length_of_simulation/5))])
+        # plt.title(str(3*int(np.floor(length_of_simulation/5))))
+        # plt.subplot(235)
+        # self.plot_history(self.history_[4*int(np.floor(length_of_simulation/5))])
+        # plt.title(str(4*int(np.floor(length_of_simulation/5))))
+        plt.subplot(236)
+        self.plot_history_alt(self.history_[-1])
+        plt.title(str(length_of_simulation))
+        plt.show()
+        plt.close()
+
+    def plot_history(self,history):
+        blue, red, coordinates_dictionary = history
+        blue_coordinates = []
+        red_coordinates = []
+        for index in blue:
+            blue_coordinates.append(coordinates_dictionary[index])
+        for index in red:
+            red_coordinates.append(coordinates_dictionary[index])
+        plt.scatter(*zip(*blue_coordinates), c = 'b')
+        plt.scatter(*zip(*red_coordinates), c = 'r')
+        plt.axis('off')
+
+    def plot_history_alt(self,history):
+        blue_coordinates, red_coordinates = history
+        plt.scatter(*zip(*blue_coordinates), c = 'b')
+        plt.scatter(*zip(*red_coordinates), c = 'r')
+        plt.axis('off')
+
+
 
 if __name__ == '__main__':
-    test = ShellingSegregation(50, 15, .5, 4, 10, 10)
+    test = ShellingSegregation(300, 200, .8, 4, 100, 100)
     test.whole_process()
-    print(test.segregation_index())
-    print(len(test.blue))
-    print(len(test.red))
+    test.plot_alt()
+    # print(test.segregation_index())
+    # print(len(test.blue))
+    # print(len(test.red))
