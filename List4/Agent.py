46eef5de8f4acbf468671c5406e254e34f5a20e3
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-11-18 18:35:19 +0100

    add method for calculation similar neighbour index and little refactor

--- List4/Agent.py
+++ List4/Agent.py
@@ -1,4 +1,4 @@
-from random import uniform, seed, randint
+from random import seed, randint
 from math import sqrt
 
 seed(10)  # for reproducible random numbers
@@ -6,15 +6,14 @@ seed(10)  # for reproducible random numbers
 
 class Agent:
 
-    def __init__(self, type, num_neighbors, require_same_type, empty_spots):
+    def __init__(self, type, num_neighbors, staying_threshold, empty_spots):
         self.type = type
         self.num_neighbors = num_neighbors
-        self.require_same_type = require_same_type
+        self.staying_threshold = staying_threshold
         self.empty_spots = empty_spots
         self.draw_location()
 
     def draw_location(self):
-        #self.location = uniform(0, 1), uniform(0, 1)
         self.location = randint(0, 100), randint(0, 100)
         while self.location not in self.empty_spots:
             self.location = randint(0, 100), randint(0, 100)
@@ -26,7 +25,7 @@ class Agent:
         b = (self.location[1] - other.location[1])**2
         return sqrt(a + b)
 
-    def happy(self, agents):
+    def fraction_of_neighbours_of_the_same_type(self, agents):
         "True if sufficient number of nearest neighbors are of the same type."
         distances = []
         # distances is a list of pairs (d, agent), where d is distance from
@@ -42,11 +41,11 @@ class Agent:
         neighbors = [agent for d, agent in distances[:self.num_neighbors]]
         # == Count how many neighbors have the same type as self == #
         num_same_type = sum(self.type == agent.type for agent in neighbors)
-        return num_same_type >= self.require_same_type
+        return num_same_type / self.num_neighbors
 
     def update(self, agents):
         "If not happy, then randomly choose new locations until happy."
         #while not self.happy(agents):
-        if not self.happy(agents):
+        if self.fraction_of_neighbours_of_the_same_type(agents) < self.staying_threshold:
             self.empty_spots.append(self.location)
             self.draw_location()
--- List4/SchellingSegregation_ver2.py
+++ List4/SchellingSegregation_ver2.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 
 class Grid:
     def __init__(self, num_of_type_0=250, num_of_type_1=250, num_neighbors_type_0=10, num_neighbors_type_1=10,
-                 require_same_type_type_0=5, require_same_type_type_1=5, num_of_rows=100, num_of_columns=100):
+                 staying_threshold_0=.5, staying_threshold_1=.5, num_of_rows=100, num_of_columns=100):
         self.num_of_type_0 = num_of_type_0
         self.num_of_type_1 = num_of_type_1
         self.num_neighbors_type_0 = num_neighbors_type_0
         self.num_neighbors_type_1 = num_neighbors_type_1
-        self.require_same_type_type_0 = require_same_type_type_0
-        self.require_same_type_type_1 = require_same_type_type_1
+        self.staying_threshold_0 = staying_threshold_0
+        self.staying_threshold_1 = staying_threshold_1
         self.num_of_rows = num_of_rows
         self.num_of_columns = num_of_columns
-        self.empty_spots = self.create_lies_of_empty_spots(self.num_of_rows, self.num_of_columns)
-        self.agents = [Agent(0, self.num_neighbors_type_0, self.require_same_type_type_0, self.empty_spots) for i in range(self.num_of_type_0)]
-        self.agents.extend(Agent(1, self.num_neighbors_type_1, self.require_same_type_type_1, self.empty_spots) for i in range(self.num_of_type_1))
+        self.empty_spots = self.create_table_of_empty_spots(self.num_of_rows, self.num_of_columns)
+        self.agents = [Agent(0, self.num_neighbors_type_0, self.staying_threshold_0, self.empty_spots) for i in range(self.num_of_type_0)]
+        self.agents.extend(Agent(1, self.num_neighbors_type_1, self.staying_threshold_1, self.empty_spots) for i in range(self.num_of_type_1))
 
-    def create_lies_of_empty_spots(self, num_of_rows, num_of_columns):
+    def create_table_of_empty_spots(self, num_of_rows, num_of_columns):
         empty_spots = []
         for i in range(num_of_rows):
             for j in range(num_of_columns):
@@ -42,6 +42,12 @@ class Grid:
                 break
         plot_distribution(self.agents, count)
         print('Converged, terminating.')
+
+    def calculate_similar_neighbour_index(self):
+        similar_neighbour_index = 0
+        for agent in self.agents:
+            similar_neighbour_index += agent.fraction_of_neighbours_of_the_same_type(self.agents)
+        return similar_neighbour_index / len(self.agents)
 
 
 def plot_distribution(agents, cycle_num):
@@ -71,9 +77,10 @@ if __name__ == '__main__':
     num_of_type_1 = 250
     num_neighbors_0 = 10
     num_neighbors_1 = 15
-    require_same_type_0 = 5
-    require_same_type_1 = 10
+    staying_threshold_0 = .5
+    staying_threshold_1 = 2 / 3
     max_number_of_iterations = 100
 
-    grid = Grid(num_of_type_0, num_of_type_1, num_neighbors_0, num_neighbors_1, require_same_type_0, require_same_type_1)
+    grid = Grid(num_of_type_0, num_of_type_1, num_neighbors_0, num_neighbors_1, staying_threshold_0, staying_threshold_1)
     grid.run_algorithm(max_number_of_iterations)
+    print(grid.calculate_similar_neighbour_index())
