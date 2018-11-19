48e037fdfa1ee7ea91b9d4dd28c62aa78cc12cdc
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-11-19 10:59:10 +0100

    main_periodic init

---
+++ List4/main_periodic.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from os.path import dirname
+sys.path.append(dirname(__file__))
+# from List4.schelling_segregation import Grid
+from schelling_segregation import GridPeriodic
+
+num_of_type_0 = int(sys.argv[1])
+num_of_type_1 = int(sys.argv[2])
+num_neighbors_0 = int(sys.argv[3])
+num_neighbors_1 = int(sys.argv[4])
+staying_threshold_0 = float(sys.argv[5])
+staying_threshold_1 = float(sys.argv[6])
+max_number_of_iterations = int(sys.argv[7])
+file_name = sys.argv[8]+'_periodic_'+('_').join(sys.argv[1:8])
+
+grid = GridPeriodic(num_of_type_0, num_of_type_1, num_neighbors_0, num_neighbors_1, staying_threshold_0, staying_threshold_1)
+grid.run_algorithm(max_number_of_iterations, plot_n_print=False)
+os.makedirs('figures', exist_ok=True)
+grid.plot('figures/'+file_name)
+grid.save_agents_to_file('figures/'+file_name)
