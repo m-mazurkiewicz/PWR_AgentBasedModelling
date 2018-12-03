58ed4993fdb641aa62f07fff34df3cb1ab889943
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-12-03 16:49:19 +0100

    Update of the architecture slowing_down added

--- List5/car.py
+++ List5/car.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 class Car:
-    def __init__(self,location,max_speed):
-        self.location = location
+    def __init__(self,max_speed):
         self.max_speed = max_speed
         self.speed = np.random.randint(0,self.max_speed+1)
--- List5/road.py
+++ List5/road.py
@@ -1,22 +1,36 @@
 import numpy as np
 import math
 from List5.car import Car
+from copy import deepcopy as copy
 
 
 class Road:
-    def __init__(self, number_of_cells, density_of_cars, slowing_down_probability, max_speed = 5):
-        self.number_of_cels = number_of_cells
-        self.number_of_cars = math.ceil(self.number_of_cels*density_of_cars)
+    def __init__(self, number_of_cells, density_of_cars, slowing_down_probability, max_speed=5):
+        self.number_of_cells = number_of_cells
+        self.number_of_cars = math.ceil(self.number_of_cells * density_of_cars)
         self.p = slowing_down_probability
         self.max_speed = max_speed
-        self.cars = [Car(location,self.max_speed) for location in np.random.choice(range(self.number_of_cels),self.number_of_cars,replace=False)]
-        self.locations = [car.location for car in self.cars]
+        self.cells = [0 for _ in range(self.number_of_cells)]
+        self.cars = [Car(self.max_speed) for _ in range(self.number_of_cars)]
+        for key, location in enumerate(
+                np.random.choice(range(self.number_of_cells), self.number_of_cars, replace=False)):
+            self.cells[location] = self.cars[key]
 
     def acceleration(self):
         for car in self.cars:
-            car.speed = max(car.speed+1,self.max_speed)
+            car.speed = min(car.speed + 1, self.max_speed)
+
+    def slowing_down(self):
+        for location, car in enumerate(self.cells):
+            if isinstance(car, Car):
+                for i in range(1, car.speed + 1):
+                    if isinstance(self.cells[(location + i) % self.number_of_cells], Car):
+                        car.speed = i - 1
+                        break
 
 
 if __name__ == '__main__':
-    r = Road(50,0.5,0.5)
+    r = Road(5000, 0.4, 0.5)
+    # print([car.speed for car in r.cells if car!=0])
     r.acceleration()
+    r.slowing_down()
