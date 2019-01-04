e3d70d688bc76f52d1aac2f8e37ec7a62a5d5611
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2019-01-04 20:51:52 +0100

    switched to 1 as initial spin

--- List7/situation_model.py
+++ List7/situation_model.py
@@ -4,24 +4,24 @@ from numpy import absolute, sum
 
 class SituationGraph:
     def __init__(self, num_agents):
-        self.spins = self.initialise_spins(num_agents,-1)
+        self.spins = self.initialise_spins(num_agents)
 
     @staticmethod
-    def initialise_spins(size, spin):
+    def initialise_spins(size):
         d = dict()
         for i in range(size):
-            d[i] = spin
+            d[i] = 1
         return d
 
-    def simulate(self,num_steps, p, replace=False, flexibility=.5, no_of_neighbours=4):
+    def simulate(self, num_steps, independence, replace=False, flexibility=.5, no_of_neighbours=4):
         for i in range(num_steps):
             node = self.select_random_node()
-            if random() > p:
+            if random() <= (1 - independence):
                 neighbors_spins = choice(list(self.spins.values()), no_of_neighbours, replace=replace)
                 if absolute(sum(neighbors_spins)) == no_of_neighbours:
                     self.spins[node] = neighbors_spins[0]
             else:
-                self.spins[node] *= (random() < flexibility) * 2 - 1
+                self.spins[node] *= (random() <= (1 - flexibility)) * 2 - 1
 
     def select_random_node(self):
         return choice(list(self.spins.keys()))
