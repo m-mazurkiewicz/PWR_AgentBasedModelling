6562ddfebfa1eac78aaf47314760b20ee38230d1
Marcin Mazurkiewicz <mmazurkiewicz95@gmail.com>
2019-01-04 14:44:21 +0100

    fixed flexibility and p

--- List7/situation_model.py
+++ List7/situation_model.py
@@ -13,15 +13,15 @@ class SituationGraph:
             d[i] = spin
         return d
 
-    def simulate(self,num_steps, replace=False, flexibility=.5, no_of_neighbours=4):
+    def simulate(self,num_steps, p, replace=False, flexibility=.5, no_of_neighbours=4):
         for i in range(num_steps):
             node = self.select_random_node()
-            if random() < flexibility:
+            if random() > p:
                 neighbors_spins = choice(list(self.spins.values()), no_of_neighbours, replace=replace)
                 if absolute(sum(neighbors_spins)) == no_of_neighbours:
                     self.spins[node] = neighbors_spins[0]
             else:
-                self.spins[node] *= round(random()) * 2 - 1
+                self.spins[node] *= (random() < flexibility) * 2 - 1
 
     def select_random_node(self):
         return choice(list(self.spins.keys()))
@@ -29,4 +29,4 @@ class SituationGraph:
 
 if __name__ == '__main__':
     graph = SituationGraph(100)
-    graph.simulate(100)
+    graph.simulate(100, .5)
